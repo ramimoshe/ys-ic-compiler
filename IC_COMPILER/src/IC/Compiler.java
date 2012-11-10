6e5e4d76f8e83af2e68bad29d65ba471ee88633e
jonerez <jonerez@gmail.com>
2012-11-10 15:45:10 +0000

    Added jars

--- IC_COMPILER/src/IC/Compiler.java
+++ IC_COMPILER/src/IC/Compiler.java
@@ -5,24 +5,24 @@ import IC.Parser.*;
 import java.io.*;
 
 public class Compiler {
-	public static void main(String[] args) {
-		// check that received one parameter
-		if (args.length == 0) {
-			System.err.println("Missing input file argument");
-			System.exit(-1);
-		}
-		Token tkn;
-		try {
-			FileReader txtFile = new FileReader(args[0]); // may throw i/o
-															// exception
-			Lexer lexer = new Lexer(txtFile);
-			do {
-				tkn = lexer.next_token();
-				System.out.println(tkn.toString());
-			} while (tkn.sym != sym.EOF);
-		} catch (Exception e) {
-			System.err.println(e);
-			System.exit(-1);
-		}
+    public static void main(String[] args) {
+	// check that received one parameter
+	if (args.length == 0) {
+	    System.err.println("Missing input file argument");
+	    System.exit(-1);
 	}
+	Token tkn;
+	try {
+	    FileReader txtFile = new FileReader(args[0]); // may throw i/o
+	    // exception
+	    Lexer lexer = new Lexer(txtFile);
+	    do {
+		tkn = lexer.next_token();
+		System.out.println(tkn.toString());
+	    } while (tkn.sym != sym.EOF);
+	} catch (Exception e) {
+	    System.err.println(e);
+	    System.exit(-1);
+	}
+    }
 }
