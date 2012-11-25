f0ed79cadb4c5e469f3422bede1f60f33f82ad5b
jonerez <jonerez@gmail.com>
2012-11-25 20:57:45 +0000

    Copied stuff from PA1

--- IC_COMPILER/PA2/src/IC/Parser/LexicalError.java
+++ IC_COMPILER/PA2/src/IC/Parser/LexicalError.java
@@ -1,8 +1,24 @@
 package IC.Parser;
 
-public class LexicalError extends Exception
-{
-    public LexicalError(String message) {
-     // do something
-    }
+public class LexicalError extends Exception {
+	private int line;
+	private String value;
+
+	public LexicalError(String message) {
+		super(message);
+	}
+
+	public LexicalError(String message, int line, String value) {
+		super(message);
+		this.line = line + 1;
+		this.value = value;
+	}
+
+	public String toString() {
+		if (this.value == null) {
+			return this.line + ": Lexical error: " + this.getMessage();
+		} else {
+			return this.line + ": Lexical error: " + this.getMessage() + " '" + this.value + "'";
+		}
+	}
 }
--- IC_COMPILER/PA2/src/IC/Parser/Token.java
+++ IC_COMPILER/PA2/src/IC/Parser/Token.java
@@ -2,8 +2,30 @@ package IC.Parser;
 
 import java_cup.runtime.Symbol;
 
+/**
+ * This class holds information for a single token that includes the token line
+ * number, ID from sym.java and its value.
+ */
 public class Token extends Symbol {
-    public Token(int id, int line) {
-        super(id, null);
-    }
+	public Token(int id, int line) {
+		// call super constructor for Symbol(int sym_num,int l,int r)
+		super(id, line + 1, 0);
+	}
+
+	public Token(int id, int line, Object value) {
+		// call super constructor for Symbol(int sym_num,int l,int r,Object
+		// value)
+		super(id, line + 1, 0, value);
+	}
+
+	public String toString() {
+		String symName = IC.Parser.sym.getSymbolName(this.sym);
+
+		String str = this.left + ": " + symName;
+		if (value != null) {
+			String valueStr = value.toString();
+			str += "(" + valueStr + ")";
+		}
+		return str;
+	}
 }
