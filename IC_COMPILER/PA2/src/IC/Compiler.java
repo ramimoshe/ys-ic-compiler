6115d770ed121046b0ab60ebf6f7c8b6f4cd8449
jonerez <jonerez@gmail.com>
2012-11-25 21:26:15 +0000

    - Deleted classes from SVN (no need for that) - Fixed issues from PA1:   * Now gracefully exits on LexicalError   * Now doesn't do integer range checking in lexer

--- IC_COMPILER/PA2/src/IC/Compiler.java
+++ IC_COMPILER/PA2/src/IC/Compiler.java
@@ -4,29 +4,31 @@ import IC.Parser.*;
 import java.io.*;
 
 public class Compiler {
-	public static void main(String[] args) {
-		// check that received one parameter
-		if (args.length == 0) {
-			System.err.println("Missing input file argument");
-			System.exit(-1);
-		}
-		Token tkn;
-		try {
-			Reader txtFile = new FileReader(args[0]);
-			Lexer lexer = new Lexer(txtFile);
-			// Read the tokens from the scanner, one by one, and print
-			// each one according to spec (Token.toString() takes care
-			// of that).
-			do {
-				tkn = lexer.next_token();
-				System.out.println(tkn.toString());
-			} while (tkn.sym != sym.EOF);
-		} catch (IOException e) {
-			System.err.println(e);
-			System.exit(-1);
-		} catch (LexicalError e) {
-			System.err.println(e);
-			System.exit(-1);
-		}
+    public static void main(String[] args) {
+	// check that received one parameter
+	if (args.length == 0) {
+	    System.err.println("Missing input file argument");
+	    System.exit(-1);
 	}
+	try {
+	    Reader txtFile = new FileReader(args[0]);
+	    Lexer lexer = new Lexer(txtFile);
+	    // Read the tokens from the scanner, one by one, and print
+	    // each one according to spec (Token.toString() takes care
+	    // of that).
+	    Token tkn;
+	    do {
+		tkn = lexer.next_token();
+		System.out.println(tkn.toString());
+	    } while (tkn.sym != sym.EOF);
+	} catch (LexicalError e) {
+	    // We were asked to gracefully return 0 on user-code related exceptions.
+	    System.out.println(e);
+	    System.exit(0);
+	} catch (IOException e) {
+	    // On other errors we do crash.
+	    System.err.println(e);
+	    System.exit(1);
+	}
+    }
 }
