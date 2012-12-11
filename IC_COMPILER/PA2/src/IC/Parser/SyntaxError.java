7a315a1cd05e2cfe362be2cabbc41147ba39ceab
jonerez <jonerez@gmail.com>
2012-12-11 11:14:49 +0000

    Improved error reporting. Added to Library.cup

--- IC_COMPILER/PA2/src/IC/Parser/SyntaxError.java
+++ IC_COMPILER/PA2/src/IC/Parser/SyntaxError.java
@@ -2,6 +2,7 @@ package IC.Parser;
 
 public class SyntaxError extends Exception {
 	private int line;
+	private String value;
 
 	public SyntaxError(String message) {
 		super(message);
@@ -12,8 +13,18 @@ public class SyntaxError extends Exception {
 		this.line = line;
 	}
 
+	public SyntaxError(String message, int line, String value) {
+		super(message);
+		this.line = line;
+		this.value = value;
+	}
+
 	public String toString() {
-		return this.line + ": Syntax error: " + this.getMessage();
+	  String msg = "Line " + this.line + ": Syntax error: " + this.getMessage();
+	  if (value != null) {
+	    msg += "; " + value;
+	  }
+		return msg;
 	}
 	
 	public int getLine() {
