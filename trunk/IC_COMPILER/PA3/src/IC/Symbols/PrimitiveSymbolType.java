de1c6b659fbff05f1ab27bb573318fae64d76c12
jonerez@gmail.com <jonerez@gmail.com@bd620822-f77e-48dd-81f1-d2178c6391e5>
2013-01-01 10:37:26 +0000

    Implemented most of SymbolTable builder visitor

---
+++ trunk/IC_COMPILER/PA3/src/IC/Symbols/PrimitiveSymbolType.java
@@ -0,0 +1,13 @@
+package IC.Symbols;
+
+import IC.AST.PrimitiveType;
+
+public class PrimitiveSymbolType extends SymbolType {
+
+	PrimitiveType type;
+
+	public PrimitiveSymbolType(PrimitiveType type) {
+		this.type = type;
+	}
+
+}
